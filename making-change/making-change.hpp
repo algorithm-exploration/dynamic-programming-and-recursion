6f7e1dfb7127c5ece83437f610c3ee959c9952ee
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:45:07 -0600

    Create making-change.hpp

---
+++ making-change/making-change.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+size_t changePossibilities(int amount, const vector<int>& denominations)
+{
+    // calculate the number of ways to make change
+    
+
+    return 0;
+}
