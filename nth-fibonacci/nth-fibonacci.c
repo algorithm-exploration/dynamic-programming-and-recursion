5ad6a7ea7186eccbd9cf4ef40f199f661ef25627
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:41:55 -0600

    Create nth-fibonacci.c

---
+++ nth-fibonacci/nth-fibonacci.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int fib(int n)
+{
+    // compute the nth Fibonacci number
+    
+
+    return 0;
+}
