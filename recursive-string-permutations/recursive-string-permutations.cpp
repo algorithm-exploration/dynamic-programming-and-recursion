e50cec20dce2836b903d48d97102ca3f8c6f5303
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:38:54 -0600

    Create  recursive-string-permutations.cpp

---
+++ recursive-string-permutations/recursive-string-permutations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+unordered_set<string> getPermutations(const string& inputString)
+{
+    // generate all permutations of the input string
+    
+
+    return unordered_set<string>();
+}
