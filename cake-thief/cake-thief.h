3b098882921acdb898dd774de0d00e3dfc09b904
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:47:46 -0600

    Create cake-thief.h

---
+++ cake-thief/cake-thief.h
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+typedef struct CakeType {
+    unsigned int weight;
+    unsigned int value;
+} CakeType;
+
+unsigned long long maxDuffelBagValue(const CakeType *cakeTypes, size_t cakeTypesLength,
+                                     unsigned int weightCapacity)
+{
+    // calculate the maximum value that we can carry
+    
+
+    return 0;
+}
