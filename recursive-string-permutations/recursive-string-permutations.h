2979ec6b1b32ed1927bb763124b5218b5bb397af
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:37:24 -0600

    Create recursive-string-permutations.h

---
+++ recursive-string-permutations/recursive-string-permutations.h
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+// Implements a set.
+// Set * setNew(void);
+// void setFree(Set *set);
+// size_t setSize(const Set* set);
+// int setContainsString(const Set *set, const char *str);
+// void setInsertString(Set *set, const char *str);
+// SetIterator setIterationStart(const Set *set);
+// SetIterator setIterationNext(SetIterator si);
+// int setIterationIsEnd(const SetIterator si);
+// char * setIteratorGetStringValue(const SetIterator si);
+#include "Cake/Set.h"
+
+Set * getPermutations(const char *inpusString)
+{
+    // generate all permutations of the input string
+    
+
+    return setNew();
+}
