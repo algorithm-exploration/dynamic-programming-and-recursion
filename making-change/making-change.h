855adfa3d8afd091a4dd4c9a5bc27c4e47fb4edb
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-22 22:46:20 -0600

    Create making-change.h

---
+++ making-change/making-change.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+size_t changePossibilities(int amount, const int *denominations, size_t denominationsLength)
+{
+    // calculate the number of ways to make change
+    
+
+    return 0;
+}
